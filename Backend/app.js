dbff3cb4fa00104de4c3072f09f7acc8c5b9ec94
Priyanshi1310 <priyanshi13gupta@gmail.com>
2025-03-11 23:15:16 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -41,6 +41,15 @@ app.use(
 
 app.options("*", cors());
 
+// Explicitly set CORS headers for all responses
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://myntraclonefullstack-ui.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
